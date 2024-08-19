937d5fe0d0b5bd028b0367bfeccd7160f61e8096
Vladimir <vladimir@sobaka.com>
2024-08-19 15:54:34 +0200

    http client init

--- src/Http.php
+++ src/Http.php
@@ -4,18 +4,128 @@ namespace Anodio\HttpClient;
 
 
 use Anodio\Core\ContainerStorage;
-use DI\Attribute\Inject;
+use Symfony\Component\HttpClient\Response\MockResponse;
 
 class Http
 {
     public static function fake(array $fakes) {
+        if (count($fakes)==0) {
+            throw new \Exception('Fakes should not be empty');
+        }
+        foreach ($fakes as $url=>$fake) {
+            if (!is_string($url)) {
+                throw new \Exception('Fakes should be an associative array with url as a key');
+            }
+            if (!($fake instanceof MockResponse)) {
+                throw new \Exception('Fakes should be an instance of MockResponse');
+            }
+        }
         $container = ContainerStorage::getContainer();
         $container->set('http.fakes', $fakes);
     }
 
-    public static function get(string $url, array $query = []) {
-        $container = ContainerStorage::getContainer();
-        $client = $container->get(\Illuminate\Http\Client\Factory::class);
-        return $client->get($url, $query);
+    public static function url(string $url): HttpRequestBuilder {
+        $builder = new HttpRequestBuilder();
+        return $builder->url($url);
     }
+
+    public static function withBody(\Psr\Http\Message\StreamInterface|string $content, string $contentType = 'text/plain'): HttpRequestBuilder {
+        $builder = new HttpRequestBuilder();
+        return $builder->withBody($content, $contentType);
+    }
+
+    public static function withJson(array $data): HttpRequestBuilder {
+        $builder = new HttpRequestBuilder();
+        return $builder->withJson($data);
+    }
+
+    public static function acceptJson(): HttpRequestBuilder {
+        $builder = new HttpRequestBuilder();
+        return $builder->acceptJson();
+    }
+
+    public static function accept(string $contentType): HttpRequestBuilder {
+        $builder = new HttpRequestBuilder();
+        return $builder->accept($contentType);
+    }
+
+    public static function withOptions(array $options): HttpRequestBuilder {
+        $builder = new HttpRequestBuilder();
+        return $builder->withOptions($options);
+    }
+
+    public static function withQueryParameters(array $parameters): HttpRequestBuilder {
+        $builder = new HttpRequestBuilder();
+        return $builder->withQueryParameters($parameters);
+    }
+
+    public static function withHeaders(array $headers): HttpRequestBuilder {
+        $builder = new HttpRequestBuilder();
+        return $builder->withHeaders($headers);
+    }
+
+    public static function withHeader(string $name, mixed $value): HttpRequestBuilder {
+        $builder = new HttpRequestBuilder();
+        return $builder->withHeader($name, $value);
+    }
+
+    public static function withTimeout(int $seconds): HttpRequestBuilder {
+        $builder = new HttpRequestBuilder();
+        return $builder->withTimeout($seconds);
+    }
+
+    public static function withConnectTimeout(int $seconds): HttpRequestBuilder {
+        $builder = new HttpRequestBuilder();
+        return $builder->withConnectTimeout($seconds);
+    }
+
+    public static function withBasicAuth(string $username, string $password): HttpRequestBuilder {
+        $builder = new HttpRequestBuilder();
+        return $builder->withBasicAuth($username, $password);
+    }
+
+    public static function withDigestAuth(string $username, string $password): HttpRequestBuilder {
+        $builder = new HttpRequestBuilder();
+        return $builder->withDigestAuth($username, $password);
+    }
+
+    public static function withToken(string $token, string $type = 'Bearer'): HttpRequestBuilder {
+        $builder = new HttpRequestBuilder();
+        return $builder->withToken($token, $type);
+    }
+
+    public static function withUserAgent(string|bool $userAgent): HttpRequestBuilder {
+        $builder = new HttpRequestBuilder();
+        return $builder->withUserAgent($userAgent);
+    }
+
+    public static function withCookies(array $cookies, string $domain): HttpRequestBuilder {
+        $builder = new HttpRequestBuilder();
+        return $builder->withCookies($cookies, $domain);
+    }
+
+    public static function maxRedirects(int $max): HttpRequestBuilder {
+        $builder = new HttpRequestBuilder();
+        return $builder->maxRedirects($max);
+    }
+
+    public static function withoutRedirecting(): HttpRequestBuilder {
+        $builder = new HttpRequestBuilder();
+        return $builder->withoutRedirecting();
+    }
+
+    public static function withoutVerifying(): HttpRequestBuilder {
+        $builder = new HttpRequestBuilder();
+        return $builder->withoutVerifying();
+    }
+
+    public static function contentType(string $contentType): HttpRequestBuilder {
+        $builder = new HttpRequestBuilder();
+        return $builder->contentType($contentType);
+    }
+
+    public static function createClient() {
+        return new HttpRequestBuilder();
+    }
+
 }
---
+++ src/HttpRequestBuilder.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace Anodio\HttpClient;
+
+use Anodio\Core\ContainerStorage;
+use Symfony\Component\HttpClient\CurlHttpClient;
+use Symfony\Component\HttpClient\MockHttpClient;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class HttpRequestBuilder
+{
+    private string $url;
+
+    private array $options;
+
+    public function url(string $url): static {
+        $this->url = $url;
+        return $this;
+    }
+
+    public function withBody(\Psr\Http\Message\StreamInterface|string $content, string $contentType = 'text/plain'): static {
+        $this->options['body'] = $content;
+        return $this;
+    }
+
+    public function withJson(array $data): static {
+        $this->options['body'] = json_encode($data);
+        $this->options['headers'] = array_merge($this->headers, [
+            'Content-Type' => 'application/json'
+        ]);
+        return $this;
+    }
+
+    public function acceptJson(): static {
+        $this->headers['Accept'] = 'application/json';
+        return $this;
+    }
+
+    public function accept(string $contentType): static {
+        $this->headers['Accept'] = $contentType;
+        return $this;
+    }
+
+    public function withOptions(array $options): static {
+        $this->options = array_merge($this->options, $options);
+        return $this;
+    }
+
+    public function withQueryParameters(array $parameters): static {
+        $this->options['query'] = $parameters;
+        return $this;
+    }
+
+    public function withHeaders(array $headers): static {
+        $this->options['headers'] = $headers;
+        return $this;
+    }
+
+    public function withHeader(string $name, mixed $value): static {
+        $this->options['headers'][$name] = $value;
+        return $this;
+    }
+
+    public function withTimeout(int $seconds): static {
+        $this->options['timeout'] = $seconds;
+        return $this;
+    }
+
+    public function withConnectTimeout(int $seconds): static {
+        $this->options['connect_timeout'] = $seconds;
+        return $this;
+    }
+
+    public function withBasicAuth(string $username, string $password): static {
+        $this->options['auth_basic'] = [$username, $password];
+        return $this;
+    }
+
+    public function withDigestAuth(string $username, string $password): static {
+        $this->options['auth_digest'] = [$username, $password];
+        return $this;
+    }
+
+    public function withToken(string $token, string $type = 'Bearer'): static {
+        $this->headers['Authorization'] = $type.' '.$token;
+        return $this;
+    }
+
+    public function withUserAgent(string|bool $userAgent): static {
+        $this->headers['User-Agent'] = $userAgent;
+        return $this;
+    }
+
+    public function withCookies(array $cookies, string $domain): static {
+        $this->options['cookies'] = $cookies;
+        $this->options['cookies_domain'] = $domain;
+        return $this;
+    }
+
+    public function maxRedirects(int $max): static {
+        $this->options['max_redirects'] = $max;
+        return $this;
+    }
+
+    public function withoutRedirecting(): static {
+        $this->options['max_redirects'] = 0;
+        return $this;
+    }
+
+    public function withoutVerifying(): static {
+        $this->options['verify_peer'] = false;
+        return $this;
+    }
+
+    public function contentType(string $contentType): static {
+        $this->headers['Content-Type'] = $contentType;
+        return $this;
+    }
+
+    public function request(string $method): \Symfony\Contracts\HttpClient\ResponseInterface
+    {
+        return $this->createClient($this->url)->request($method, $this->url, $this->options);
+    }
+
+    public function get(): \Symfony\Contracts\HttpClient\ResponseInterface {
+        return $this->createClient($this->url)->request('get', $this->url, $this->options);
+    }
+
+    public function head(): \Symfony\Contracts\HttpClient\ResponseInterface
+    {
+        return $this->createClient($this->url)->request('head', $this->url, $this->options);
+    }
+
+    public function post(): \Symfony\Contracts\HttpClient\ResponseInterface
+    {
+        return $this->createClient($this->url)->request('post', $this->url, $this->options);
+    }
+
+    public function patch(): \Symfony\Contracts\HttpClient\ResponseInterface
+    {
+        return $this->createClient($this->url)->request('patch', $this->url, $this->options);
+    }
+
+    public function put(): \Symfony\Contracts\HttpClient\ResponseInterface
+    {
+        return $this->createClient($this->url)->request('put', $this->url, $this->options);
+    }
+
+    public function delete(): \Symfony\Contracts\HttpClient\ResponseInterface
+    {
+        return $this->createClient($this->url)->request('delete', $this->url, $this->options);
+    }
+
+    private function createClient(?string $url=null): HttpClientInterface {
+        if (!$url) {
+            throw new \Exception('You forgot to specify url');
+        }
+        $container = ContainerStorage::getContainer();
+        if ($container->has('http.fakes')) {
+            $fakes = $container->get('http.fakes');
+            $fakesFound = [];
+            foreach ($fakes as $fakeUrl=>$fake) {
+                if (str_starts_with($url, $fakeUrl)) {
+                    $faksesFound[$fakeUrl] = $fake;
+                    unset($fakes[$fakeUrl]);
+                }
+            }
+            if (count($fakesFound)>0) {
+                $container->set('http.fakes', $fakes);
+                return new MockHttpClient($fakesFound, $url);
+            } else {
+                return new CurlHttpClient([
+                    'base_uri' => $url
+                ]);
+            }
+        } else {
+            return new CurlHttpClient([
+                'base_uri' => $url
+            ]);
+        }
+    }
+}
